4560062bc6a6e1f139c2cac20e7e75dc67704b16
Alyssa Palmares <alyssapalmares@yahoo.com>
2021-08-15 22:31:46 +0800

    added process.env.PORT again :)

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ if (process.env.MONGO_ENV === "local") {
         });
 }
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
