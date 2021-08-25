4eed248b0d1717b17f5a17c750c514f3a9cb1353
Alyssa Palmares <alyssapalmares@yahoo.com>
2021-08-25 18:10:29 +0800

    updated Profile Activity
    - profile picture not loading
    - fix colors on app bar and navigation view
    - add listeners for buttons
    - add listeners for navigation items

--- alintana/app/src/main/java/com/mobdeve/s17/dizon/palmares/alintana/ProfileActivity.kt
+++ alintana/app/src/main/java/com/mobdeve/s17/dizon/palmares/alintana/ProfileActivity.kt
@@ -1,15 +1,19 @@
 package com.mobdeve.s17.dizon.palmares.alintana
 
 import android.content.res.ColorStateList
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.isEmpty
 import android.util.Log
-import com.mobdeve.s17.dizon.palmares.alintana.databinding.ActivityMainBinding
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
 import com.mobdeve.s17.dizon.palmares.alintana.databinding.ActivityProfileBinding
 import com.mobdeve.s17.dizon.palmares.alintana.model.User
+import com.squareup.picasso.Picasso
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding : ActivityProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +23,54 @@ class ProfileActivity : AppCompatActivity() {
 
         Log.v("USER", user!!.toString())
 
-        binding.tvText.text = "Hello " + user.username
-        binding.progressBar.progress = 50
-        binding.progressBar.progressTintList = ColorStateList.valueOf(resources.getColor(R.color.primary))
-        setContentView(binding.root)
+        binding.tvProfileUsername.text = user.username
+        binding.tvProfileHeadline.text = user.headline
 
+        if(user.userImage != null && !isEmpty(user.userImage))
+            Picasso.get().load(user.userImage).into(binding.ivProfileImage)
+        else
+            binding.ivProfileImage.setImageResource(R.drawable.ic_baseline_person_24)
+
+        binding.tvProfileAge.text = user.getAge().toString()
+        binding.tvProfileSex.text =  user.sex
+        binding.tvProfileLoc.text =  user.location
+
+        if(user.sex.equals("Prefer Not to Say", true)){
+            binding.tvProfileSep1.visibility = View.GONE
+            binding.tvProfileSex.visibility = View.GONE
+        }
+        if(isEmpty(user.location)){
+            binding.tvProfileSep2.visibility = View.GONE
+            binding.tvProfileLoc.visibility = View.GONE
+        }
+
+        binding.tvProfileLvl.text = (user.experience/100).toString()
+        binding.progressBar.progress = user.experience%100
+        binding.progressBar.progressTintList = ColorStateList.valueOf(resources.getColor(R.color.primary))
+
+        // toolbar
+        setSupportActionBar(binding.toolbarProfile)
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.dlProfile,
+            binding.toolbarProfile,
+            R.string.openNavDrawer,
+            R.string.closeNavDrawer
+        )
+
+        binding.dlProfile.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        binding.navView.setNavigationItemSelectedListener(this)
+
+        setContentView(binding.root)
     }
 
+    // navigation bar functions
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return false
+    }
+    override fun onPointerCaptureChanged(hasCapture: Boolean) {
+        TODO("Not yet implemented")
+    }
 }
