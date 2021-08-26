8b6fa8901b74e0244b1a4b7a02ff2099fe240bd8
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2021-08-26 14:31:18 +0800

    di ko na alam

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,14 +1,19 @@
 const User = require("../models/user");
-
+const mongoose = require("mongoose");
 exports.getUser = async (req, res, next) => {
     try {
+        console.log(req.params.id);
+        // const data = await User.findOne({ _id: req.params.id });
+
         const data = await User.findById(req.params.id);
-        console.log(data);
+
+        console.log("DATA", data);
 
         if (data == null) throw "No User";
 
         res.status(200).json(data);
     } catch (err) {
+        console.log(err);
         res.status(500).json({
             status: "Error",
             message: "Error",
