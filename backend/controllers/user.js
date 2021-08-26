c92d1b5e447b6b2fe489fe6826a681036b8490cb
Alyssa Palmares <alyssapalmares@yahoo.com>
2021-08-26 18:08:18 +0800

    Revert "di ko na alam"
    This reverts commit 8b6fa8901b74e0244b1a4b7a02ff2099fe240bd8.

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,19 +1,14 @@
 const User = require("../models/user");
-const mongoose = require("mongoose");
+
 exports.getUser = async (req, res, next) => {
     try {
-        console.log(req.params.id);
-        // const data = await User.findOne({ _id: req.params.id });
-
         const data = await User.findById(req.params.id);
-
-        console.log("DATA", data);
+        console.log(data);
 
         if (data == null) throw "No User";
 
         res.status(200).json(data);
     } catch (err) {
-        console.log(err);
         res.status(500).json({
             status: "Error",
             message: "Error",
